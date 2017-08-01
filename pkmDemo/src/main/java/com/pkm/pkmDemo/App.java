4907132878003874c7f7c49d2dbbe4e69872879a
liqiangd <xqliulang@163.com>
2017-08-01 15:07:48 +0800

    创建demo工程

---
+++ pkmDemo/src/main/java/com/pkm/pkmDemo/App.java
@@ -0,0 +1,13 @@
+package com.pkm.pkmDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
